061f124760982aa15ba748b35d3b87bef27f4948
JadeMara <45374080+JadeMara@users.noreply.github.com>
2018-11-28 09:06:52 +0100

    Add files via upload

---
+++ MJ_assignment1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 15:35:10 2018
+
+@author: Mara
+"""
+
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+l.reverse()
+
+l
+
+string = "bottles of beer on the wall"
+
+for song in l:
+    print (song, string)
